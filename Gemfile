081b3616e5e4ec72772215423e071a1930c2555b
Rhommel Lamas <roml@rhommell.com>
2016-03-08 12:53:19 +1100

    Ensures SSL certificates can be removed if ensure => absent
    At the moment when `ensure => 'absent'` the module deletes the directory, then it tries to delete the files too which puppet won't be able to find. This PR fixes this bug.

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rake'
+gem 'mocha'
+gem 'diff-lcs'
+gem 'json_pure'
+gem 'json'
+gem 'metadata-json-lint'
+gem 'puppet-lint', '1.1.0'
+gem 'rspec-puppet', '2.2.0'
+gem 'puppet', '>4'
+gem 'puppetlabs_spec_helper', '0.10.3'
--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -1,19 +1,24 @@
 define ssl::certificate(
   $source,
-  $ensure = 'present',
+  $ensure      = 'present',
   $private_key = 'private_key.key',
-  $all_in_one = '',
-  $owner = 'root',
-  $group = 'root',
+  $all_in_one  = '',
+  $owner       = 'root',
+  $group       = 'root',
 ) {
 
   file { "/etc/ssl/${name}/":
     ensure  => $ensure ? {
       present => 'directory',
+      absent  => 'absent',
       default => $ensure,
     },
     source  => $source,
-    recurse => true,
+    recurse => $ensure ? {
+      present => true,
+      absent  => false,
+      default => true,
+    },
     purge   => true,
     force   => true,
     owner   => $owner,
@@ -21,21 +26,23 @@ define ssl::certificate(
     mode    => '0644',
   }
 
-  file { "/etc/ssl/${name}/${private_key}":
-    ensure => $ensure,
-    source => "${source}/${private_key}",
-    owner  => $owner,
-    group  => $group,
-    mode   => '0640',
-  }
-
-  if ($all_in_one) {
-    file { "/etc/ssl/${name}/${all_in_one}":
-      ensure => $ensure,
-      source => "${source}/${all_in_one}",
+  if $ensure == 'present' {
+    file { "/etc/ssl/${name}/${private_key}":
+      ensure => 'present',
+      source => "${source}/${private_key}",
       owner  => $owner,
       group  => $group,
       mode   => '0640',
+    }
+
+    if $all_in_one {
+      file { "/etc/ssl/${name}/${all_in_one}":
+        ensure => 'present',
+        source => "${source}/${all_in_one}",
+        owner  => $owner,
+        group  => $group,
+        mode   => '0640',
+      }
     }
   }
 }
